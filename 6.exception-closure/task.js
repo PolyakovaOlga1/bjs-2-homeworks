303c1f682828596200ad0f00750e08b90c01662e
olga.poliakova <olga.sam2002@gmail.com>
2026-07-14 21:09:36 +0400

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿"use strict";
+
+function parseCount(value){
+	const parsedValue = Number.parseFloat(value);
+	if (Number.isNaN(parsedValue)) {
+	  throw new Error("Невалидное значение");
+	}
+	return parsedValue;
+  }
+  
+  function validateCount(value) {
+	try {
+	  return parseCount(value);
+	} catch (error) {
+	  return error;
+	}
+  }
+  
+  class Triangle {
+	constructor(a, b, c) {
+	  if ( (a + b) <= c || (b + c) <= a || (a + c) <= b) {
+		throw new Error("Треугольник с такими сторонами не существует");
+	  }
+	  this.a = a;
+	  this.b = b;
+	  this.c = c;
+	}
+	get perimeter() {
+	  return Number(this.a + this.b + this.c);
+	}
+	get area() {
+	  const p = this.perimeter / 2;
+	  const areaValue = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+	  return Number(areaValue.toFixed(3));
+	}
+  }
+  
+  function getTriangle(a, b, c) {
+	try {
+	  return new Triangle(a, b, c);
+	} catch (error) {
+	  return {
+		get perimeter() {
+		  return "Ошибка! Треугольник не существует";
+		},
+		get area() {
+		  return "Ошибка! Треугольник не существует";
+		}
+	  }
+	}
+  }
